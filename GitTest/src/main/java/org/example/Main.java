25661dc92f1cc2975fee6c2dfeebae22bf57ec5a
MCorder00 <maxdellcorder@gmail.com>
2024-04-09 15:10:45 -0500

    I finished my thought.

--- GitTest/src/main/java/org/example/Main.java
+++ GitTest/src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("This is my 1337 new app!");
+        System.out.println("It will disrupt all industries!");
+        System.out.println("HACKING INTO NATO. . . DONE");
     }
 }
