3898576fcaf9582505415906996818eb4e88a635
MCorder00 <maxdellcorder@gmail.com>
2024-04-09 15:11:45 -0500

    I had some more thoughts.

--- GitTest/src/main/java/org/example/Main.java
+++ GitTest/src/main/java/org/example/Main.java
@@ -5,5 +5,8 @@ public class Main {
         System.out.println("This is my 1337 new app!");
         System.out.println("It will disrupt all industries!");
         System.out.println("HACKING INTO NATO. . . DONE");
+
+        System.out.println("THIS IS THE MOST POWERFUL APP IN EXISTENCE");
+        System.out.println("I WILL BECOME MORE POWERFUL THAN ZUCKERBERG EVER IMAGINED");
     }
 }
