ae4fd20f0be6e9cf8a9e3d58b04fcebebebfff65
MCorder00 <maxdellcorder@gmail.com>
2024-04-12 16:44:19 -0500

    Deleted unnecessary variables

--- Workshops/FinancialCalculator/src/main/java/org/example/Main.java
+++ Workshops/FinancialCalculator/src/main/java/org/example/Main.java
@@ -11,9 +11,7 @@ public class Main {
 
     public static void main(String[] args) {
         // declarations in main for handlers
-        char startOver = 'Y';
         boolean shouldContinue = true;
-
         // nested try/catch inside do/while loop to restart program
         do {
             try {
